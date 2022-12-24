9886248a268fd323b5a3c6211b354473f63ef044
minhtri1990 <minhtri08clc@gmail.com>
2022-12-24 12:36:23 +0700

    update resource

--- src/main/java/com/entrance/controller/ResourceController.java
+++ src/main/java/com/entrance/controller/ResourceController.java
@@ -1,17 +1,21 @@
 package com.entrance.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceController {
 
-    @RequestMapping("/user")
+    @Operation(description = "user domain")
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String getUser() {
         return "Hello User";
     }
 
-    @RequestMapping("/admin")
+    @Operation(description = "admin domain")
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String getAdmin() {
         return "Hello Admin";
     }
